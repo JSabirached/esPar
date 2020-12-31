cc110b003db616e3d6d1d079a75a91bb0664f2ac
JSabirached <josueabirached@gmail.com>
2020-12-31 14:32:57 -0300

    guardado

---
+++ esPar.js
@@ -0,0 +1,7 @@
+let parOimpar= function(numero){
+	numero %2==0 ? console.log("El numero es par") : console.log("El numero es impar");
+}
+console.log(parOimpar(8));
+
+  // if (numero ===0){ 
+//    return "debe ingresar numeros distinto de cero"}
